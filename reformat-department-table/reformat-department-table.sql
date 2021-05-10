661774f7b5114668a8e4d5753083618acb5a406f
Yung Chi Daniel Cho <danielairy10@gmail.com>
2021-05-10 12:13:46 -0400

    Time: 572 ms (24.81%), Space: 0B (100.00%) - LeetHub

--- reformat-department-table/reformat-department-table.sql
+++ reformat-department-table/reformat-department-table.sql
@@ -1,17 +1,17 @@
 # Write your MySQL query statement below
-SELECT id,
-       sum(CASE WHEN month ="Jan" THEN revenue END) AS Jan_Revenue,
-       sum(CASE WHEN month ="Feb" THEN revenue END) AS Feb_Revenue,
-       sum(CASE WHEN month ="Mar" THEN revenue END) AS Mar_Revenue,
-       sum(CASE WHEN month ="Apr" THEN revenue END) AS Apr_Revenue,
-       sum(CASE WHEN month ="May" THEN revenue END) AS May_Revenue,
-       sum(CASE WHEN month ="Jun" THEN revenue END) AS Jun_Revenue,
-       sum(CASE WHEN month ="Jul" THEN revenue END) AS Jul_Revenue,
-       sum(CASE WHEN month ="Aug" THEN revenue END) AS Aug_Revenue,
-       sum(CASE WHEN month ="Sep" THEN revenue END) AS Sep_Revenue,
-       sum(CASE WHEN month ="Oct" THEN revenue END) AS Oct_Revenue,
-       sum(CASE WHEN month ="Nov" THEN revenue END) AS Nov_Revenue,
-       sum(CASE WHEN month ="Dec" THEN revenue END) AS Dec_Revenue
+Select id,
+MAX(Case WHEN month = "Jan" THEN revenue Else Null END) As Jan_Revenue,
+MAX(Case WHEN month = "Feb" THEN revenue Else Null END) As Feb_Revenue,
+MAX(Case WHEN month = "Mar" THEN revenue Else Null END) As Mar_Revenue,
+MAX(Case WHEN month = "Apr" THEN revenue Else Null END) As Apr_Revenue,
+MAX(Case WHEN month = "May" THEN revenue Else Null END) As May_Revenue,
+MAX(Case WHEN month = "Jun" THEN revenue Else Null END) As Jun_Revenue,
+MAX(Case WHEN month = "Jul" THEN revenue Else Null END) As Jul_Revenue,
+MAX(Case WHEN month = "Aug" THEN revenue Else Null END) As Aug_Revenue,
+MAX(Case WHEN month = "Sep" THEN revenue Else Null END) As Sep_Revenue,
+MAX(Case WHEN month = "Oct" THEN revenue Else Null END) As Oct_Revenue,
+MAX(Case WHEN month = "Nov" THEN revenue Else Null END) As Nov_Revenue,
+MAX(Case WHEN month = "Dec" THEN revenue Else Null END) As Dec_Revenue
 
 FROM Department
 GROUP BY id
